99a7663815b191af20f32d9302bc57d053facb48
sedsedus <sedsedus1@gmail.com>
2022-07-07 23:18:46 +0200

    Refactor and optimization

--- main.cpp
+++ main.cpp
@@ -6,15 +6,12 @@
 using Mitype = double;
 using Mtype = std::complex<Mitype>;
 
-int mandelbrot(Mtype c, Mitype limit, int maxIterations)
+int mandelbrot(Mtype c, int maxIterations)
 {
     Mtype m = 0;
     auto i = 0;
-    for (i = 0; i < maxIterations; ++i) {
+    for (i = 0; i < maxIterations && std::abs(m) < 2; ++i) {
         m = m * m + c;
-        if (std::abs(m) > limit) {
-            return i;
-        }
     }
     return i;
 }
@@ -48,23 +45,25 @@ void SetZoomAt(sf::RenderWindow &window, sf::Vector2i pos, float zoom)
     view.move(offsetCoords);
     window.setView(view);
 }
-
 int main()
 {
     auto constexpr width = 1000;
     auto constexpr height = 1000;
+    auto const maxColorValue = 255;
     auto planeXSize = 2.0;
     auto planeYSize = 2.0;
     auto planeXCenter = 0.0;
     auto planeYCenter = 0.0;
-    auto maxIterations = 10;
-    auto const maxValue = 10000;
-
-    auto maxColorValue = 255;
+    auto maxIterations = 200;
     sf::RenderWindow window(sf::VideoMode(width, height), "Mandelbrot");
 
     sf::VertexArray pts;
     pts.resize(width * height);
+    for (auto x = 0; x < width; ++x) {
+        for (auto y = 0; y < height; ++y) {
+            pts[x + height * y] = sf::Vertex({ static_cast<float>(x), static_cast<float>(y) }, sf::Color::White);
+        };
+    }
     while (window.isOpen()) {
         // handle events
         sf::Event event;
@@ -92,31 +91,31 @@ int main()
                     planeXSize *= 1.1;
                     planeYSize *= 1.1;
                 } else if (event.type == sf::Event::MouseWheelMoved) {
-                    float dzoom = static_cast<float>(-event.mouseWheel.delta);
+                    // float dzoom = static_cast<float>(-event.mouseWheel.delta);
                 }
             }
         }
 
         auto mapToSize = [](auto v, auto size, auto planeCenter, auto planeSize) {
-            return mapToRange(v, 0.0, static_cast<Mitype>(size), planeCenter - planeSize/2, planeCenter + planeSize/2);
+            return mapToRange(v, 0.0, static_cast<Mitype>(size), planeCenter - planeSize / 2, planeCenter + planeSize / 2);
         };
-
+        auto getColor = [maxIterations, maxColorValue](int iterations) {
+            auto scaled1 = mapToRange((Mitype)iterations, 0.0, (Mitype)maxIterations, (Mitype)maxColorValue, 0.0);
+            auto scaled2 = mapToRange((Mitype)iterations, 0.0, (Mitype)maxIterations, (Mitype)100, 0.0);
+            auto scaled3 = mapToRange((Mitype)iterations, 0.0, (Mitype)maxIterations, (Mitype)maxColorValue, 0.0);
+            return sf::Color(scaled1, scaled2, scaled3);
+        };
         for (auto x = 0; x < width; ++x) {
             for (auto y = 0; y < height; ++y) {
-                sf::Color color;
                 auto xScaled = mapToSize(static_cast<Mitype>(x), static_cast<Mitype>(width), planeXCenter, planeXSize);
                 auto yScaled = mapToSize(static_cast<Mitype>(y), static_cast<Mitype>(height), planeYCenter, planeYSize);
                 Mtype p { xScaled, yScaled };
-                if (std::abs(p) > 2) {
-                    color = sf::Color::White;
-                    // printf("X(x,y): (%0.2f, %0.2f)\n",xScaled,yScaled);
-                } else {
-                    auto iterations = mandelbrot(p, maxValue, maxIterations);
-                    auto scaled = mapToRange((Mitype)iterations, 0.0, (Mitype)maxIterations, (Mitype)maxColorValue, 0.0);
-                    color = sf::Color(scaled, scaled, scaled);
-                    // printf("(x,y,it): (%0.2f, %0.2f, %d)\n",xScaled,yScaled,iterations);
-                }
-                pts[x + height*y] = (sf::Vertex({ static_cast<float>(x), static_cast<float>(y) }, color));
+                auto iterations = mandelbrot(p, maxIterations);
+                // auto scaled = mapToRange((Mitype)iterations, 0.0, (Mitype)maxIterations, (Mitype)maxColorValue, 0.0);
+                auto color = getColor(iterations);
+                // color = sf::Color(scaled, scaled, 1/2*scaled);
+                // printf("(x,y,it): (%0.2f, %0.2f, %d)\n",xScaled,yScaled,iterations);
+                pts[x + height * y].color = color;
                 // printf("%d: %0.2f\n", i, mandelbrot(i, 5).real());
             }
         }
