2aae67e23e12bbcc973f68e1885fcfa9e541e939
sedsedus <tomek_hav@interia.pl>
2022-07-08 19:08:08 +0200

    Add jet color scale
     - needs refactor

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "mandelbrot.h"
-
+#include <cstdio>
 
 int main()
 {
-    Mandelbrot m(1000,1000);
+    Mandelbrot m(1000, 1000);
     return m.run();
+    for (int i = 0; i < 31; ++i) {
+        auto c = m.getColor(i, 30);
+        printf("%-2d | %-3d, %-3d, %-3d\n",i,  c.r, c.g, c.b);
+    }
 }
--- mandelbrot.h
+++ mandelbrot.h
@@ -5,6 +5,8 @@ class Mandelbrot
 public:
     Mandelbrot(int width, int height);
     int run();
+    static sf::Color getColor(int iterations, int maxIterations);
+    static sf::Color getColorOld(int iterations, int maxIterations);
 
 private:
     using Mitype = double;
@@ -13,11 +15,10 @@ private:
     int ptToIdx(int x, int y) const;
     int mandelbrot(Mtype c, int maxIterations) const;
 
-    sf::Color getColor(int iterations, int maxIterations) const;
     const int mWidth;
     const int mHeight;
-    int mMaxIterations = 200;
-    sf::Vector2<double> mPlaneSize { 2.0, 2.0 };
+    int mMaxIterations = 35;
+    sf::Vector2<double> mPlaneSize { 3.0, 3.0 };
     sf::Vector2<double> mPlaneCenter { 0.0, 0.0 };
     static auto constexpr maxColorValue = 255;
 };
