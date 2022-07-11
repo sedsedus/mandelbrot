e044d5e4690401fbb4bc7444f49fcb4b88520d4e
sedsedus <tomek_hav@interia.pl>
2022-07-11 22:27:55 +0200

    Increase the starting iterations

--- mandelbrot.h
+++ mandelbrot.h
@@ -28,7 +28,7 @@ private:
     void calcMandelbrot(sf::VertexArray &pts);
     const int mWidth;
     const int mHeight;
-    int mMaxIterations = 10;
+    int mMaxIterations = 100;
     sf::Vector2<Mitype> mPlaneSize { 3.0, 3.0 };
     sf::Vector2<Mitype> mPlaneCenter { 0.0, 0.0 };
     static auto constexpr maxColorValue = 255;
