0e179d0df2a0968151868e661107e381a91478e6
sedsedus <sedsedus1@gmail.com>
2022-08-15 19:03:58 +0200

    offset the initial view

--- mandelbrot.h
+++ mandelbrot.h
@@ -32,7 +32,7 @@ private:
     const int mHeight;
     int mMaxIterations = 100;
     sf::Vector2<float> mPlaneSize { 3.0, 3.0 };
-    sf::Vector2<float> mPlaneCenter { 0.0, 0.0 };
+    sf::Vector2<float> mPlaneCenter { -0.6, 0.0 };
     std::array<sf::Glsl::Vec4, CONFIG_ITERATION_LIMIT> mVec4Colors;
     static auto constexpr maxColorValue = 255;
 };
